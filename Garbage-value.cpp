c9ba646c1c67fd81ecd1e8d9ae87db6f78face23
Rohit Thakur <105488212+its-me-rohit@users.noreply.github.com>
2022-06-11 22:54:30 +0530

    Create Garbage-value.cpp

---
+++ Garbage-value.cpp
@@ -0,0 +1,23 @@
+What is the output
+
+
+What will be the output of the following code ?
+
+
+int arr[100];
+cout << arr[0];
+
+
+
+Options
+This problem has only one correct answer
+
+(1) 0
+
+(2)Garbage value
+
+(3)Error
+
+
+
+Correct Answer = Garbage value
